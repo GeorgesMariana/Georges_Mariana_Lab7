be6823adfca64ab2676d9c5c7febc3a37a762b5e
Georges Mariana <georges.mariana@outlook.com>
2023-12-03 17:18:26 +0200

    Laboratorul8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace Georges_Mariana_Lab7
+﻿using System;
+using Georges_Mariana_Lab7.Data;
+using System.IO;
+
+namespace Georges_Mariana_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
